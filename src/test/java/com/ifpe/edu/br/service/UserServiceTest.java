f07dc5b7cdc7de1069a89fd9b41b4dc912b73b48
MabiGermano <beatriz.germano@nubank.com.br>
2022-01-19 22:17:26 -0300

    creating general instances to test

--- src/test/java/com/ifpe/edu/br/service/UserServiceTest.java
+++ src/test/java/com/ifpe/edu/br/service/UserServiceTest.java
@@ -9,23 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
-    private User criarUsuario(){
-        Address address = Address.builder().city("Recife")
-                .country("Brasil")
-                .district("dos bobos")
-                .street("Rua dos bobos")
-                .state("PE")
-                .number("11")
-                .zipcode("25698965").build();
 
-        return User.builder()
-                .address(address)
-                .email("mbges@discente.ifpe.edu.br")
-                .name("Maria")
-                .birthDate(new Date("06/03/1997"))
-                .bioDescription("estudante de Análise e desenvolvimento de sistemas")
-                .identityRegistration("58965986536")
-                .password("ZSE$xdr5").build();
-    }
 
 }
